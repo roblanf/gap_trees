0635eb28d12dfff0ca18b78d253343ee50051ec2
roblanf <rob.lanfear@gmail.com>
2019-07-16 15:27:20 +1000

    code cleaning in runIQtree.R
    still a fair bit to do

--- runIQtree.R
+++ runIQtree.R
@@ -1,102 +1,108 @@
 # This function extracts some of the main results from an IQtree run output file.
 
-runIQtree <- function(sdata, format = 'phylip', aadata = aadata, temp_name, iqtreePath = '~/Downloads/iqtree-1.7-beta9-MacOSX/bin/iqtree', model = 'JC', tree = NULL){
-       
-       require(phangorn)
+runIQtree <- function(iqtreePath, run_ID){
 
-    if(format == 'fasta'){
-        if(aadata) d <- read.aa(sdata, format = 'fasta') else d <- read.dna(sdata, format = 'fasta')
-        fileName <- gsub('fasta', 'phy', sdata)
-        write.dna(d, file = fileName)
-    } else if(format == 'bin'){
-        write.dna(sdata, file = temp_name)
-        fileName <- temp_name
-    } else if(format == "nexus"){
-        if(aadata) d <- as.AAbin(read.nexus.data(sdata)) else d <- as.DNAbin(read.nexus.data(sdata))
-        fileName <- gsub('nexus', 'phy', sdata)
-        write.dna(d, file = fileName)
-    } else {
-        fileName <- sdata
-    }
-    print(paste("Locus", fileName, "was read successfully."))
+    aln_name = paste0(run_ID, ".fasta")
 
-    if(is.null(tree)){
-        iqtreeCommand = paste0(iqtreePath, " -s ", fileName, " -m ", model, " -alrt 1000")
-    } else {
-        treenumber <- round(runif(1, min = 1000, max = 9999))
-	treefile <- paste0("temp.", treenumber, ".tre")
-        write.tree(tree, file = treefile)
-        iqtreeCommand = paste0(iqtreePath, " -s ", fileName, " -m ", model, " -g ", treefile, " -alrt 1000")
-    }
+    # run IQtree without a tree and with 100 bootstraps
+    iqc1 = paste0(iqtreePath, " -s ", aln_name, " -m JC -alrt 1000 -b 100 ", " -pre ", run_ID)
+    system(iqc1)
 
-    system(iqtreeCommand)
-    if(!is.null(tree)) system(paste0("rm ", treefile))
+    # run IQtree forcing all three resolutions
+    iqc2 = paste0(iqtreePath, " -s ", aln_name, " -m JC -g tree_A", treefile, " -pre ", paste0(run_ID, "tree_A"))
+    system(iqc2)
+    iqc3 = paste0(iqtreePath, " -s ", aln_name, " -m JC -g tree_B", treefile, " -pre ", paste0(run_ID, "tree_B"))
+    system(iqc3)
+    iqc4 = paste0(iqtreePath, " -s ", aln_name, " -m JC -g tree_C", treefile, " -pre ", paste0(run_ID, "tree_C"))
+    system(iqc4)
     
+    
+    # We return a single-row data frame with the following entries
+
+    # 0. The run_ID (this so we can later merge with the parameter table)
+    # 1. The identity of the tree from the IQ-TREE search ("A", "B", "C", see the treefiles in the repo)
+    # 2. The lnL of the tree from the IQ-TREE search
+    # 3. The bootsrap value on the internal branch for the IQ-TREE search
+    # 4. The tree string for the tree from the IQ-TREE search (in case we want any other information)
+    # 5. The lnL of the tree_A analysis
+    # 6. The lnL of the tree_B analysis
+    # 7. The lnL of the tree_C analysis
+    # 8-10. The internal branch lengths of the A, B, and C trees
+    # 11-13. The t1 branch length of the A, B, and C trees
+    # 14-16. The t2 branch length of the A, B, and C trees
+    # 17-19. The t3 branch length of the A, B, and C trees
+    # 20-22. The t4 branch length of the A, B, and C trees 
+
+
+
+
+
+    # get the tree from the search
     allout <- readLines(paste0(fileName, ".iqtree"))
 
-	torm <- grep(paste0(fileName, c(".ckp.gz", ".bionj", ".log", ".mldist", ".treefile", ".uniqueseq.", ".contree", ".splits.nex", ".iqtree", ".parstree"), collapse = "|"), dir(), value = T)
-	
-	for(i in torm) system(paste0("rm ", i))
-       
-       res <- list()
-       
-       esttree <- read.tree(text = allout[grep("Tree in newick format:", allout)[1] + 2])
+  	torm <- grep(paste0(fileName, c(".ckp.gz", ".bionj", ".log", ".mldist", ".treefile", ".uniqueseq.", ".contree", ".splits.nex", ".iqtree", ".parstree"), collapse = "|"), dir(), value = T)
+  	
+  	for(i in torm) system(paste0("rm ", i))
+         
+      res <- list()
 
-       boots <- as.numeric(esttree$node.label)
-       res$meanNodeSupport <- mean(boots, na.rm = T)
-       
-       res$nodeSupport95 <- diff(quantile(boots, c(0.025, 0.975), na.rm = T))
+      esttree <- read.tree(text = allout[grep("Tree in newick format:", allout)[1] + 2])
 
-       trlen <- grep("Total tree length", allout, value = T)
-       res$treeLength <- as.numeric(gsub(".* ", "", trlen))
+      boots <- as.numeric(esttree$node.label)
+      res$meanNodeSupport <- mean(boots, na.rm = T)
 
-       if(length(grep("HKY|GTR", model))==1){
-        	piLocation <- grep('State frequencies', allout)+2
-        	piParams <- as.numeric(gsub('.* ', '', allout[(piLocation):(piLocation+3)]))
-        	res$piParams <-  piParams
-       }
+      res$nodeSupport95 <- diff(quantile(boots, c(0.025, 0.975), na.rm = T))
 
-       if(length(grep("[+]G", model))==1){
-        	alphaLocation <- grep('Gamma shape alpha', allout)
-        	alphaParam <- as.numeric(gsub(".* ", "", allout[alphaLocation]))
-       		res$alphaParam <- alphaParam
-       }
+      trlen <- grep("Total tree length", allout, value = T)
+      res$treeLength <- as.numeric(gsub(".* ", "", trlen))
 
-       if(length(grep("GTR", model))==1){
-        	gtrLocation <- grep('Rate parameter R', allout)+2
-        	gtrMatrix <- as.numeric(gsub('.* ', '',  allout[(gtrLocation):(gtrLocation+5)]))
-        	res$gtrMatrix <-  gtrMatrix
-       }
+      if(length(grep("HKY|GTR", model))==1){
+      	piLocation <- grep('State frequencies', allout)+2
+      	piParams <- as.numeric(gsub('.* ', '', allout[(piLocation):(piLocation+3)]))
+      	res$piParams <-  piParams
+      }
 
-       if(length(grep("HKY", model))==1){
-        	trtvLocation <- grep('A-G:', allout)
-        	trtvRatio <- as.numeric(gsub(".* ", "", allout[trtvLocation]))
-        	res$trtvRatio <-  trtvRatio
-       }
+      if(length(grep("[+]G", model))==1){
+      	alphaLocation <- grep('Gamma shape alpha', allout)
+      	alphaParam <- as.numeric(gsub(".* ", "", allout[alphaLocation]))
+      		res$alphaParam <- alphaParam
+      }
 
-       likse <- grep("Log-likelihood of the tree:", allout, value = T)
-       res$likelihood <- as.numeric(gsub(".*:|[(].*| ", "", likse))
-       
-       unclik <- grep("Unconstrained log-likelihood", allout, value = T) 
-       res$uncLikelihood <- as.numeric(gsub(".* ", "", unclik))
-       
-       res$delta <- res$uncLikelihood - res$likelihood
-       
-       res$tree <- esttree
-       
-       # Other parameters that might be of interest in the future
-       #params <- grep("Number of free parameters", allout, value  = T)
-       #res$Nparams <- gsub(".* ", "", params)       
-       #aic <- grep("Akaike information criterion", allout, value = T)
-       #res$aic <- gsub(".* ", "", aic[2])
-       #res$aicc <- gsub(".* ", "", aic[3])       
-       #bic <- grep("Bayesian information criterion", allout, value = T)
-       #res$bic <- gsub(".* ", "", bic[2])       
-       #bootrange <- range(boots, na.rm = T)
-       #res$bootmax <- max(bootrange)
-       #res$bootmin <- min(bootrange)
-       #res$bootrangesize <- diff(bootrange)       
-       #res$likelihoodSE <- gsub(".*[(]|[)].*|.* ", "", likse)
+      if(length(grep("GTR", model))==1){
+      	gtrLocation <- grep('Rate parameter R', allout)+2
+      	gtrMatrix <- as.numeric(gsub('.* ', '',  allout[(gtrLocation):(gtrLocation+5)]))
+      	res$gtrMatrix <-  gtrMatrix
+      }
 
-       return(res)
+      if(length(grep("HKY", model))==1){
+      	trtvLocation <- grep('A-G:', allout)
+      	trtvRatio <- as.numeric(gsub(".* ", "", allout[trtvLocation]))
+      	res$trtvRatio <-  trtvRatio
+      }
+
+      likse <- grep("Log-likelihood of the tree:", allout, value = T)
+      res$likelihood <- as.numeric(gsub(".*:|[(].*| ", "", likse))
+
+      unclik <- grep("Unconstrained log-likelihood", allout, value = T) 
+      res$uncLikelihood <- as.numeric(gsub(".* ", "", unclik))
+
+      res$delta <- res$uncLikelihood - res$likelihood
+
+      res$tree <- esttree
+
+      # Other parameters that might be of interest in the future
+      #params <- grep("Number of free parameters", allout, value  = T)
+      #res$Nparams <- gsub(".* ", "", params)       
+      #aic <- grep("Akaike information criterion", allout, value = T)
+      #res$aic <- gsub(".* ", "", aic[2])
+      #res$aicc <- gsub(".* ", "", aic[3])       
+      #bic <- grep("Bayesian information criterion", allout, value = T)
+      #res$bic <- gsub(".* ", "", bic[2])       
+      #bootrange <- range(boots, na.rm = T)
+      #res$bootmax <- max(bootrange)
+      #res$bootmin <- min(bootrange)
+      #res$bootrangesize <- diff(bootrange)       
+      #res$likelihoodSE <- gsub(".*[(]|[)].*|.* ", "", likse)
+
+      return(res)
 }
